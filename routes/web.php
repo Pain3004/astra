890439e4df8e24c3d59dafb68ba0246d0ec22a37
bhagwati <team@veravalonline.com>
2023-04-11 18:02:39 +0530

    B

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\DriverController;
 use App\Http\Controllers\Admin\UserController;
